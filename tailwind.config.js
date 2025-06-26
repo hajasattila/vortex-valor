45a441346e10cebe70a76412bd5c9a176f431403
Hajas Attila <hajaattila@gmail.com>
2025-06-26 03:12:34 +0200

    [MINOR] tailwind.config.js
    - VV color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vv: {
+          bgDark: '#1b1f24',       // background
+          borderDark: '#0e2b3a',   // border
+          innerBg: '#0f3d4a',      // inner
+          cyanLight: '#1fd2eb',    // spiral ligh
+          cyanMid: '#17b6cf',      // spiral dark
+          cyanGlow: '#36e3ff',     // glow
+        }
+      }
+    },
   },
   plugins: [],
 }
